b2580b65c6f830b3081d7925e784a3077e7b384b
Suraj Mane <sm3031972@gmail.com>
2026-09-01 17:01:38 +0530

    Time: 0 ms (100%), Space: 42 MB (52.51%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,8 +1,18 @@
 class Solution {
     public int fib(int n) {
+        int[] f = new int[n+1];
+        return fibonacii(n,f);
+    }
+    public int fibonacii(int n,int f[]){
         if(n == 0 || n==1){
             return n;
         }
-        return fib(n-1) + fib(n-2);
+
+        if(f[n] != 0){
+            return f[n];
+        }
+        
+        f[n] = fibonacii(n-1,f) + fibonacii(n-2,f);
+        return f[n];
     }
 }
