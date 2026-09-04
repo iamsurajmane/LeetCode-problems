91fbf66925db473635de9ffa0613f6c38e3ad013
Suraj Mane <sm3031972@gmail.com>
2026-09-04 11:53:35 +0530

    Time: 5 ms (38.34%), Space: 48.2 MB (5.24%) - LeetHub

---
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int deleteAndEarn(int[] nums) {
+      int frq[] = new int[10002];
+      int dp[] = new int[10002];
+
+      for(int i=0;i<nums.length;i++){
+        frq[nums[i]]++;
+      }
+
+      dp[0] = 0;
+      dp[1] = 1 * frq[1];
+      for(int i=2;i<10002;i++){
+        dp[i] = Math.max(dp[i-1] , dp[i-2] + i*frq[i]);
+      }
+
+      return dp[10001];
+    }
+}
